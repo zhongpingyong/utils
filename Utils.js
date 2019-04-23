595d62b55e4d54df53ea80ca943dc22c3cc6deaf
zhongpingyong <zhongpingyong88@163.com>
2019-04-23 09:29:36 +0800

    fomatTime 传入时间戳 以现在的时间戳为基准返回 多久之前

--- Utils.js
+++ Utils.js
@@ -68,5 +68,51 @@ export default {
         }catch (e) {
             console.log(e)
         }
+    },
+    /*
+    * 参数 ：valueTime 时间戳
+    * 返回值： 以现在为基准 多久之前
+    *
+    *
+    * */
+    fomatTime(valueTime){
+        if(valueTime){
+            let newData =  Date.parse(new Date());
+            let diffTime = Math.abs(newData-valueTime);
+            if (diffTime > 7 * 24 * 3600 * 1000) {
+                let date = new Date(valueTime);
+                let y = date.getFullYear();
+                let m = date.getMonth() + 1;
+                m = m < 10 ? ('0' + m) : m;
+                let d = date.getDate();
+                d = d < 10 ? ('0' + d) : d;
+                let h = date.getHours();
+                h = h < 10 ? ('0' + h) : h;
+                let minute = date.getMinutes();
+                let second = date.getSeconds();
+                minute = minute < 10 ? ('1' + minute) : minute;
+                second = second < 10 ? ('0' + second) : second;
+                return  m + '-' + d+' '+h+':'+minute;
+
+            } else if (diffTime < 7 * 24 * 3600 * 1000 && diffTime > 24 * 3600 * 1000) {
+                // //注释("一周之内");
+
+                // let time = newData - diffTime;
+                let dayNum = Math.floor(diffTime / (24 * 60 * 60 * 1000));
+                return dayNum + "天前";
+
+            } else if (diffTime < 24 * 3600 * 1000 && diffTime > 3600 * 1000) {
+                // //注释("一天之内");
+                // let time = newData - diffTime;
+                let dayNum = Math.floor(diffTime / (60 * 60 * 1000));
+                return dayNum + "小时前";
+
+            } else if (diffTime < 3600 * 1000 && diffTime > 0) {
+                // //注释("一小时之内");
+                // let time = newData - diffTime;
+                let dayNum = Math.floor(diffTime / (60 * 1000));
+                return dayNum + "分钟前";
+            }
+        }
     }
 }
